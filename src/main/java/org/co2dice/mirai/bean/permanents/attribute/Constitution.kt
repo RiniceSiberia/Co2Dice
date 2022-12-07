df7fc7f3a635ccbf191dc2e474434ea0279af1d9
trasgl <2979897510@qq.com>
2022-12-07 11:12:37 +0800

    1

--- src/main/java/org/co2dice/mirai/bean/permanents/attribute/Constitution.kt
+++ src/main/java/org/co2dice/mirai/bean/permanents/attribute/Constitution.kt
@@ -3,6 +3,8 @@ package org.co2dice.mirai.bean.permanents.attribute
 import org.co2dice.mirai.bean.dice.DiceList
 
 //体质
-class Constitution (list: DiceList) : AbstractAttributePoint("Constitution", list){
+class Constitution() : AbstractAttributePoint() {
+    override val id: String = "Constitution"
+    override var max: Int = 20
 
 }
