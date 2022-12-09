5f9e3593c930adf61f4bd7768a44b0166a264164
trasgl <2979897510@qq.com>
2022-12-09 17:09:24 +0800

    1

--- src/main/java/org/co2dice/mirai/utils/DiceUtils.kt
+++ src/main/java/org/co2dice/mirai/utils/DiceUtils.kt
@@ -34,49 +34,54 @@ object DiceUtils {
     fun getExpectDice(e:Double): DiceList {
         val dices = MutableDiceList(mutableListOf(),mutableListOf())
         //循环遍历将所有的骰子都加入到dices,计算出所有的可能性，再将dices清空，算出最接近期望值的样子
-        val exceptList = mutableListOf<Double>()
+        var exceptList = mutableListOf<Temp>()
+        //优先级和骰子的映射和期望值
         var f = true
         while (f){
             for (dice in getListByPriority()){
-                //获取当前的遍历数
+                //计算dices中所有骰子的期望值
                 dices.mutable.add(dice.dice)
                 val odds:Map<Int,Double> = dices.expected
-                //概率
-                val min = dices.mutableMin
-                val max = dices.mutableMax
-                for (i in min..max){
-                    exceptList.set(dice.priority,
-                        fun(): Any {
-                        return if (odds[i] != null) {
-                            odds[i]!! * i + exceptList[dice.priority]
-                        } else {
-                            exceptList[dice.priority]
-                        }
-                    }.invoke() as Double)
+                //所有和的概率
+                //获取一个总的期望值，即每个key乘以每个value后得到的总和
+                var value:Double = 0.0;
+                for (u in odds){
+                    value += u.key * u.value
                 }
+                //向excpetList中添加一个临时对象，然后清空dices
+                exceptList.add(Temp(dice.dice,dice.priority,value))
                 dices.mutable.remove(dice.dice)
             }
-            //现在，exList中承载了权重从高到低，每个权重下的期望值，需要从中
-            val resultList = mutableListOf<Dice>()
-            //获取到了所有的期望值
+            //现在，exList中承载了权重从高到低，每个权重下的期望值以及对应的骰子
+            val resultList = mutableListOf<Temp>()
             for (i in 0 until exceptList.size){
-                if (exceptList[i] > e - 0.5 && exceptList[i] < e + 0.5){
-                    resultList.add(getListByPriority()[i].dice)
+                //对所有骰子进行遍历，找出最接近期望值的骰子
+                if (exceptList[i].value > e - 0.5 && exceptList[i].value < e + 0.5){
+                    //如果骰子的期望值在e+-0.5的范围内，那么就将这个骰子加入到resultList中
+                    resultList.add(exceptList[i])
                 }
             }
             //现在，resultList中承载了所有的期望值在e+-0.5之间的骰子
             //如果resultList为空，则说明没有找到，将概率最接近的骰子加入到dices中
             if (resultList.isEmpty()){
                 //将和期望值最接近的骰子丢进去，注意，不是最大的骰子，而是最接近的骰子
-                dices.diceList.add(getListByPriority()[exceptList.indexOf(exceptList.stream().sorted(
-                    Comparator.comparingDouble { abs(it - e) } ).findFirst().get())].dice)
+                resultList.add(resultList.stream().sorted(Comparator.comparingDouble{
+                    return@comparingDouble abs(it.value - e)
+                }).findFirst().get())
+//                dices.diceList.add(getListByPriority()[exceptList.indexOf(exceptList.stream().sorted(
+//                    Comparator.comparingDouble { abs(it - e) } ).findFirst().get())].dice)
             }else{
-                //如果不为空，则获取resultList的第一个元素，丢入dices中，结束循环
-                dices.diceList.add(resultList[0])
+                //如果不为空，则获取一个期望值最高的元素，丢入dices中，结束循环
+                val r :Temp = resultList.stream().sorted(
+                    Comparator.comparingInt<Temp?> { it.priority }.reversed() ).findFirst().get()
+                dices.diceList.add(r.dice)
                 f = false
             }
         }
         return dices
+    }
+
+    private class Temp(var dice: Dice, var priority: Int, var value: Double) {
     }
 
     /**
