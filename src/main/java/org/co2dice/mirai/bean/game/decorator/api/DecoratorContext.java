9ab2db4ecd308523634b7f042ce0a9a6bab46870
trasgl <2979897510@qq.com>
2023-01-13 17:33:12 +0800

    1

--- src/main/java/org/co2dice/mirai/bean/game/decorator/api/DecoratorContext.java
+++ src/main/java/org/co2dice/mirai/bean/game/decorator/api/DecoratorContext.java
@@ -10,5 +10,6 @@ package org.co2dice.mirai.bean.game.decorator.api;
  */
 //作用为装饰器的传参
     //如果你把decorator当做一个function call，context就是parameter，value instance就是return value
+        //记录装饰器的逻辑，如要装饰一个成就（钻石护身），那么value instance就是成就的逻辑（当xx部位穿戴xx物品时触发），返回xxx
 public interface DecoratorContext<C extends Record & DecoratorContext<C>> {
 }
--- src/main/java/org/co2dice/mirai/bean/game/decorator/api/DecoratorValueInstance.java
+++ src/main/java/org/co2dice/mirai/bean/game/decorator/api/DecoratorValueInstance.java
@@ -10,5 +10,6 @@ package org.co2dice.mirai.bean.game.decorator.api;
  */
 //作用为装饰器的返回值
     //如果你把decorator当做一个function call，context就是parameter，value instance就是return value
+    //记录装饰器的数据，如要装饰一个成就（钻石护身），那么value instance就是成就的数据（胸甲部位），（钻石）
 public interface DecoratorValueInstance<V extends Record & DecoratorValueInstance<V>> {
 }
